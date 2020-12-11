7abe5246a835039be056b0bfb1add2cc6d4af2bf
Kyle Bi <2210722462@qq.com>
2020-12-11 09:35:58 +0800

    Add files via upload

--- model_dual_lstm_6.py
+++ model_dual_lstm_6.py
@@ -117,17 +117,17 @@ class CaptionGeneratin(nn.Module):
 
         # Only for Generation
         self.triples_embedding = nn.Embedding(triple_size, embedd_dim)
-        self.decode_step = nn.LSTMCell(embedd_dim + embedd_dim + encoder_dim, lstm_hidden_dim)
+        self.decode_step = nn.LSTMCell(embedd_dim + lstm_hidden_dim + encoder_dim, lstm_hidden_dim)
         self.decode_ds_step = nn.LSTMCell(embedd_dim + embedd_dim, lstm_hidden_dim)
-        self.init_h1_cap = nn.Linear(encoder_dim, lstm_hidden_dim)  # linear layer to find initial hidden state of LSTMCell
+        self.init_h1_cap = nn.Linear(encoder_dim,
+                                     lstm_hidden_dim)  # linear layer to find initial hidden state of LSTMCell
         self.init_c1_cap = nn.Linear(encoder_dim, lstm_hidden_dim)
-        self.init_h2_cap = nn.Linear(embedd_dim, lstm_hidden_dim)  # linear layer to find initial hidden state of LSTMCell
+        self.init_h2_cap = nn.Linear(embedd_dim,
+                                     lstm_hidden_dim)  # linear layer to find initial hidden state of LSTMCell
         self.init_c2_cap = nn.Linear(embedd_dim, lstm_hidden_dim)
         self.fc_trisize = nn.Sequential(
-            nn.Linear(lstm_hidden_dim, lstm_hidden_dim),
-            nn.ReLU(),
-            nn.Dropout(p=0.1),
-            nn.Linear(lstm_hidden_dim, triple_size),
+            nn.Dropout(p=dropout),
+            nn.Linear(lstm_hidden_dim, triple_size)
         )
         self.fc_vocab = nn.Sequential(
             nn.Dropout(p=dropout),
@@ -157,7 +157,8 @@ class CaptionGeneratin(nn.Module):
         c1 = self.init_c2_cap(related_words_embeddings)  # (batch_size, lstm_hidden_dim)
         return h1, c1, h2, c2
 
-    def forward(self, encoder_out, encoded_triples, decode_lengths, captions_embedding, related_words_embeddings):#, relations_embeddings
+    def forward(self, encoder_out, encoded_triples, decode_lengths, captions_embedding,
+                related_words_embeddings):  # , relations_embeddings
         '''
         :param encoder_out:  batch_size, 14, 14, encoder_dim
         :param decode_lengths: decode_lengths
@@ -176,7 +177,6 @@ class CaptionGeneratin(nn.Module):
         # new
         ds_predictions = torch.zeros(batch_size, max(decode_lengths), triple_size).to(device)
         caption_predictions = torch.zeros(batch_size, max(decode_lengths), vocab_size).to(device)
-        h_similiar = torch.zeros(batch_size, max(decode_lengths), self.embedd_dim).to(device)
         end_score = torch.zeros(batch_size, max(decode_lengths)).to(device)
         alphas = torch.zeros(batch_size, max(decode_lengths), num_pixels).to(device)
 
@@ -188,23 +188,26 @@ class CaptionGeneratin(nn.Module):
             awe = gate_enc * awe
 
             ''' if we should use a Linear before getting rws'''
-            score = self.graph_weight(torch.bmm(related_words_embeddings[:batch_size_t], h1[:batch_size_t].unsqueeze(-1)).squeeze(-1))  # batch_size, iter_times
-            rws = (related_words_embeddings[:batch_size_t] * score.unsqueeze(-1)).sum(dim=1)  # batch_size, embedding_dim
+            score = self.graph_weight(
+                torch.bmm(related_words_embeddings[:batch_size_t], h1[:batch_size_t].unsqueeze(-1)).squeeze(
+                    -1))  # batch_size, iter_times
+            rws = (related_words_embeddings[:batch_size_t] * score.unsqueeze(-1)).sum(
+                dim=1)  # batch_size, embedding_dim
 
             h1, c1 = self.decode_ds_step(torch.cat([triples_embedding[:batch_size_t, t, :], rws], dim=1),
-                                    (h1[:batch_size_t], c1[:batch_size_t]))
+                                         (h1[:batch_size_t], c1[:batch_size_t]))
 
             h2, c2 = self.decode_step(torch.cat([captions_embedding[:batch_size_t, t, :], h1, awe], dim=1),
-                                       (h2[:batch_size_t], c2[:batch_size_t]))
+                                      (h2[:batch_size_t], c2[:batch_size_t]))
 
             triple_preds = self.fc_trisize(h1)
             preds = self.fc_vocab(h2)
             ds_predictions[:batch_size_t, t, :] = triple_preds
             caption_predictions[:batch_size_t, t, :] = preds
             alphas[:batch_size_t, t, :] = alpha
-            h_similiar[:batch_size_t, t, :] = h2
-            end_score[:batch_size_t, t] = torch.exp(preds[:, 7463] + triple_preds[:, 5282])
-        return caption_predictions, ds_predictions, end_score, alphas, h_similiar
+            end_score[:batch_size_t, t] = torch.exp(preds[:, 7463]) + torch.exp(triple_preds[:, 5282])
+        return caption_predictions, ds_predictions, end_score, alphas
+
 
 class Graph_Encoding(nn.Module):
     def __init__(self, vocab_size, dis_size, encoder_dim, embedd_dim, dropout, hidden_size, iter_times,
@@ -239,7 +242,6 @@ class Graph_Encoding(nn.Module):
         self.ROOTupdimension.bias.data.fill_(0)
         self.ROOTupdimension.weight.data.uniform_(-0.1, 0.1)
 
-
     def initialize_graph(self, root_node_idx, root_node_idxs, new_wprob_start_from_ck):
         root_node_idxs[:, 0] = root_node_idx
         new_wprob_start_from_ck[:, 0, :] = self.adjacent_words_embedding(root_node_idx)
@@ -257,7 +259,8 @@ class Graph_Encoding(nn.Module):
         batch_size = root_node_idx.size(0)
         root_node_idxs = torch.LongTensor(batch_size * [self.iter_times * [0]]).to(device)
         distance_embedding_from_ck = torch.zeros(batch_size, self.iter_times, self.embedd_dim).to(device)
-        distance_embedding_from_ck[:, 0, :] = dis_embeddings(torch.LongTensor(batch_size * [10]).to(device))  # batch_size, embedding_dim
+        distance_embedding_from_ck[:, 0, :] = dis_embeddings(
+            torch.LongTensor(batch_size * [10]).to(device))  # batch_size, embedding_dim
         new_wprob_start_from_ck = torch.zeros(batch_size, self.iter_times, self.vocab_size).to(device)
 
         # print('initialize_graph ing....')
@@ -266,7 +269,8 @@ class Graph_Encoding(nn.Module):
             # print('Iteration {}'.format(i))
             root_nodes_embeddings = words_embeddings(root_node_idxs[:, i])
             # print('root_nodes_embeddings: ',root_nodes_embeddings.size())
-            root_input = torch.cat([self.ROOTupdimension(root_nodes_embeddings), self.IMGdedimension(mlp_out)], dim=1)  # batch_size, 1024 + 512 = 1536
+            root_input = torch.cat([self.ROOTupdimension(root_nodes_embeddings), self.IMGdedimension(mlp_out)],
+                                   dim=1)  # batch_size, 1024 + 512 = 1536
             # print('root_input: ',root_input.size())
             distance = self.distance_2_1(root_input)  # batch_size, dis_size
             # print('distance1: ',distance.size())
@@ -276,26 +280,28 @@ class Graph_Encoding(nn.Module):
             # print('distance_embedding: ',distance_embedding.size())
             related_input = torch.cat([root_input, distance_embedding], dim=1).unsqueeze(1).unsqueeze(1)
             # print('related_input: ',related_input.size())
-            p_related_words = self.relatedWordsClassifier(related_input).squeeze()  # batch_size, vocab_size  (logSoftmax)
+            p_related_words = self.relatedWordsClassifier(
+                related_input).squeeze()  # batch_size, vocab_size  (logSoftmax)
             # print('p_related_words: ',p_related_words.size())
             prob_from_gloabl_dt_graph = new_wprob_start_from_ck[:, i, :]  # batch_size, vocab_size
             flag = 1e-5 * (prob_from_gloabl_dt_graph < 0.0001) + 1.0 * (prob_from_gloabl_dt_graph >= 0.0001)
-            p_related_words = p_related_words.unsqueeze(0)
+            if p_related_words.size(0) == 7464: p_related_words = p_related_words.unsqueeze(0)
             p_related_words *= flag
             p_related_words = self.softmax(p_related_words)
 
             _, related_word_idx = p_related_words.topk(1, 1, True, True)  # batch_size, 1
-            #print(related_word_idx)
+            # print(related_word_idx)
             root_node_idxs[:, i + 1] = related_word_idx.squeeze(1)
             distance_embedding_from_ck[:, i + 1, :] = distance_embedding
             new_wprob_start_from_ck[:, i + 1, :] = self.adjacent_words_embedding(related_word_idx.squeeze(1))
 
-        return root_node_idxs, distance_embedding_from_ck
+        return root_node_idxs[:, 1:], distance_embedding_from_ck[:, 1:]
+
 
 class DecoderModel(nn.Module):
     def __init__(self, words_embeddings_from_graph, dis_embeddings_from_graph,
                  adjTable, triple_size, vocab_size, dis_size, iter_times=10, embedd_dim=512, encoder_dim=2048,
-                 hidden_size=512, attention_dim=800, dropout=0.5):#, radjTable
+                 hidden_size=512, attention_dim=800, dropout=0.5):  # , radjTable
         super(DecoderModel, self).__init__()
         # print('Initialization of DecoderModel...')
         self.vocab_size = vocab_size
@@ -309,12 +315,20 @@ class DecoderModel(nn.Module):
         self.adjacent_words_embedding = nn.Embedding.from_pretrained(adjTable)
 
         self.root_classifier = RootClassifier(vocab_size=vocab_size)
-        self.graph_left_encoding = Graph_Encoding(vocab_size,dis_size, encoder_dim, embedd_dim, dropout, hidden_size, iter_times, self.adjacent_words_embedding)
-        self.graph_right_encoding = Graph_Encoding(vocab_size, dis_size, encoder_dim, embedd_dim, dropout, hidden_size, iter_times, self.adjacent_words_embedding)
-        self.graph_self_attention = selfAttention(embedd_dim)
+        self.graph_left_encoding = Graph_Encoding(vocab_size, dis_size, encoder_dim, embedd_dim, dropout, hidden_size,
+                                                  iter_times, self.adjacent_words_embedding)
+        self.graph_right_encoding = Graph_Encoding(vocab_size, dis_size, encoder_dim, embedd_dim, dropout, hidden_size,
+                                                   iter_times, self.adjacent_words_embedding)
+        self.graph_left_attention = selfAttention(embedd_dim)
+        self.graph_right_attention = selfAttention(embedd_dim)
         self.generation = CaptionGeneratin(vocab_size=vocab_size, triple_size=triple_size, dis_size=dis_size,
                                            lstm_hidden_dim=hidden_size, attention_dim=attention_dim,
                                            embedd_dim=embedd_dim)
+        self.init_weights()
+
+    def init_weights(self):
+        self.graph_self_attention.bias.data.fill_(0)
+        self.graph_self_attention.weight.data.uniform_(-0.1, 0.1)
 
     def MyMultiply(self, news_idx, all_idx):
         '''
@@ -349,28 +363,38 @@ class DecoderModel(nn.Module):
         3. related words -> according to global graph -> ns_words  # batch_size, num_related_words, vocab_size
         4. related words -> according to global graph -> relations  # batch_size, num_related_words, relation_size
         '''
-        #print('Graph_encoding processing...')
-        left_root_node_idxs, left_distance_embedding_from_ck = self.graph_left_encoding(root_node_idx, out_root, self.words_embedding, self.dis_embedding)
-        right_root_node_idxs, right_distance_embedding_from_ck = self.graph_right_encoding(root_node_idx, out_root, self.words_embedding, self.dis_embedding)
+        # print('Graph_encoding processing...')
+        left_root_node_idxs, left_distance_embedding_from_ck = self.graph_left_encoding(root_node_idx, out_root,
+                                                                                        self.words_embedding,
+                                                                                        self.dis_embedding)
+        right_root_node_idxs, right_distance_embedding_from_ck = self.graph_right_encoding(root_node_idx, out_root,
+                                                                                           self.words_embedding,
+                                                                                           self.dis_embedding)
 
         left_root_node_embeddings = self.words_embedding(left_root_node_idxs)
         right_root_node_embeddings = self.words_embedding(right_root_node_idxs)
 
-        left_words_embeddings = torch.add(left_root_node_embeddings, left_distance_embedding_from_ck)  # batch_size, iter_times, embedding_dim
-        right_words_embeddings = torch.add(right_root_node_embeddings, right_distance_embedding_from_ck)  # batch_size, iter_times, embedding_dim
-
+        left_graph_embeddings = torch.add(left_root_node_embeddings,
+                                          left_distance_embedding_from_ck)  # batch_size, iter_times, embedding_dim
+        right_graph_embeddings = torch.add(right_root_node_embeddings,
+                                           right_distance_embedding_from_ck)  # batch_size, iter_times, embedding_dim
         '''why we use self-attention'''
-        graph_embeddings = self.graph_self_attention(q=left_words_embeddings, k=right_words_embeddings, v=right_words_embeddings, mask=None)  # batch_size, iter_times, embedding_dim
+        left_focus_graph_embeddings = self.graph_left_attention(q=left_graph_embeddings, k=right_graph_embeddings,
+                                                                v=right_graph_embeddings,
+                                                                mask=None)  # batch_size, iter_times, embedding_dim
+        right_focus_graph_embeddings = self.graph_right_attention(q=right_graph_embeddings, k=left_graph_embeddings,
+                                                                  v=left_graph_embeddings,
+                                                                  mask=None)  # batch_size, iter_times, embedding_dim
+        graph_embeddings = torch.cat([left_focus_graph_embeddings, right_focus_graph_embeddings], dim=1)
+        caption_predictions, ds_predictions, end_score, alphas = self.generation(out_caption, encoded_triples,
+                                                                                 decode_lengths, captions_embedding,
+                                                                                 graph_embeddings)
 
-        # for i in range(layer):
-        #     graph_embeddings = self.graph_self_attention(q=graph_embeddings, k=graph_embeddings, v=graph_embeddings, mask=None)
-        #print('Generation processing...')
-        caption_predictions, ds_predictions, end_score, alphas, h_similiar = self.generation(out_caption, encoded_triples, decode_lengths, captions_embedding, graph_embeddings)
+        end_score = pack_padded_sequence(end_score, [l for l in decode_lengths], batch_first=True).data
+        end_score = end_score.mean() if end_score.mean() < 10 else torch.FloatTensor([1.5]).to(device)
 
-        end_score = pack_padded_sequence(end_score, [l - 1 for l in decode_lengths], batch_first=True).data
-        end_score = end_score.mean() if end_score.mean() < 10 else torch.FloatTensor([10]).to(device)
-
-        root_node_embedding = self.words_embedding(root_node_idx)  # batch_size, embedd_dim
-        hidden_vector = h_similiar.sum(dim=1)  # batch_size, hidden_dim
-        root_loss = torch.FloatTensor([1]).to(device)-(torch.cosine_similarity(root_node_embedding, hidden_vector, eps=1e-6).mean())
+        root_node_embedding = self.words_embedding(root_node_idx)
+        hidden_vector = captions_embedding.mean(dim=1).clone().detach()  # batch_size, max_length, hidden_dim
+        root_loss = (torch.LongTensor([1]).to(device) - torch.cosine_similarity(root_node_embedding, hidden_vector,
+                                                                                eps=1e-8)).mean()
         return caption_predictions, ds_predictions, end_score, root_loss, encoded_captions, encoded_triples, decode_lengths, alphas, sort_ind
